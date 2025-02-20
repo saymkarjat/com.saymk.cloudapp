5c9adeca138fa5283ea95616f5981c1e905d43a1
saymkarjat <saymkarjat@gmail.com>
2025-02-20 17:48:13 +0300

    added SecurityConfig.java and implementation for UserDetails and UserDetailsService

---
+++ src/main/java/com/saymk/cloud6x/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.saymk.cloud6x.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+}
---
+++ src/main/java/com/saymk/cloud6x/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.saymk.cloud6x.security;
+
+import com.saymk.cloud6x.model.User;
+import com.saymk.cloud6x.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User undefined.");
+        }
+        return new UserDetailsImpl(user.get());
+    }
+}
