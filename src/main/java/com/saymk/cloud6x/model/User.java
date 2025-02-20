a0f176c572f9447232807fc600202153a7e85605
saymkarjat <saymkarjat@gmail.com>
2025-02-20 17:46:25 +0300

    added User model and enumeration Role

---
+++ src/main/java/com/saymk/cloud6x/model/User.java
@@ -0,0 +1,24 @@
+package com.saymk.cloud6x.model;
+
+import com.saymk.cloud6x.security.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "users")
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role")
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+}
---
+++ src/main/java/com/saymk/cloud6x/security/Role.java
@@ -0,0 +1,5 @@
+package com.saymk.cloud6x.security;
+
+public enum Role {
+    USER, ADMIN, GUEST
+}
