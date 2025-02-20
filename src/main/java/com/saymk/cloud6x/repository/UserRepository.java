503ec35b34dfcaa22c9030fc6b07787f897ef54b
saymkarjat <saymkarjat@gmail.com>
2025-02-20 17:47:26 +0300

    added UserRepository

---
+++ src/main/java/com/saymk/cloud6x/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.saymk.cloud6x.repository;
+
+import com.saymk.cloud6x.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
