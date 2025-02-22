b890631d7f78f1b24683cdf0916e5469d15a5931
saymkarjat <saymkarjat@gmail.com>
2025-02-23 02:52:48 +0300

    added JwtFilter extending OncePerRequestFilter to intercept requests using JWT.

---
+++ src/main/java/com/saymk/cloud6x/security/JwtFilter.java
@@ -0,0 +1,59 @@
+package com.saymk.cloud6x.security;
+
+import com.saymk.cloud6x.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import java.io.IOException;
+import java.util.Optional;
+@Component
+@AllArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Optional<String> token = extractTokenFromCookies(request);
+        if (token.isEmpty()) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String jwtToken = token.get();
+        UserDetails user;
+        try {
+            user = userDetailsService.loadUserByUsername(jwtService.extractUsername(jwtToken));
+        } catch (UsernameNotFoundException e) {
+            SecurityContextHolder.clearContext();
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (jwtService.isValid(jwtToken, user)) {
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(user, null);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private Optional<String> extractTokenFromCookies(HttpServletRequest req) {
+        Cookie accessToken = WebUtils.getCookie(req, "access_token");
+        if (accessToken == null) {
+            return Optional.empty();
+        }
+        return Optional.of(accessToken.getValue());
+    }
+}
