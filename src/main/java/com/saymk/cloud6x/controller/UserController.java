d994b47d235e1f8d414138370c31088d66cebd0b
saymkarjat <saymkarjat@gmail.com>
2025-03-03 17:27:18 +0300

    added UserController.java

---
+++ src/main/java/com/saymk/cloud6x/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.saymk.cloud6x.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @GetMapping("me")
+    public ResponseEntity<?> me(@AuthenticationPrincipal UserDetails user) {
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        Map<String, String> responseBody = Map.of("username", user.getUsername());
+        return ResponseEntity.ok().body(responseBody);
+    }
+}
