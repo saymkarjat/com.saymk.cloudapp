4300654fb5c3bb808efb0faad716448066e4106d
saymkarjat <saymkarjat@gmail.com>
2025-03-03 17:36:33 +0300

    added MinioConfig.java and configured MiniClient bean

---
+++ src/main/java/com/saymk/cloud6x/config/MinioConfig.java
@@ -0,0 +1,46 @@
+package com.saymk.cloud6x.config;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+    @Value("${minio.endpoint}")
+    private String endpoint;
+
+    @Value("${minio.user}")
+    private String user;
+
+    @Value("${minio.password}")
+    private String password;
+
+    @Value("${minio.bucket-name}")
+    private String bucket;
+
+    @Bean
+    @SneakyThrows
+    public MinioClient minioClient() {
+        MinioClient client = MinioClient
+                .builder()
+                .endpoint(endpoint)
+                .credentials(user, password)
+                .build();
+
+        boolean bucketExists = client.bucketExists(BucketExistsArgs.builder()
+                .bucket(bucket)
+                .build());
+
+        if (!bucketExists) {
+            client.makeBucket(MakeBucketArgs.builder()
+                    .bucket(bucket)
+                    .build());
+        }
+
+        return client;
+    }
+}
