18f2f87f2fba66e0c5788d7692cb31e6d6812a31
saymkarjat <saymkarjat@gmail.com>
2025-03-26 01:42:53 +0300

    feat method for modify storageSize for user

--- src/main/java/com/saymk/cloud6x/repository/UserRepository.java
+++ src/main/java/com/saymk/cloud6x/repository/UserRepository.java
@@ -2,9 +2,17 @@ package com.saymk.cloud6x.repository;
 
 import com.saymk.cloud6x.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByUsername(String username);
+
+    @Modifying
+    @Query("UPDATE User u SET u.storageSize = u.storageSize + :size WHERE u.id = :userId AND u.storageSize + :size <= :maxCapacity")
+    int increaseStorageSize(@Param("userId") Long userId, @Param("size") Long size, @Param("maxCapacity") Long maxCapacity);
 }
