c6234d4d9eba921299fd88d6eddaf3a1d59706a9
saymkarjat <saymkarjat@gmail.com>
2025-03-26 01:48:18 +0300

    refactor: improved resource management in MinIO service

--- src/main/java/com/saymk/cloud6x/minio/service/MinioService.java
+++ src/main/java/com/saymk/cloud6x/minio/service/MinioService.java
@@ -1,13 +1,16 @@
 package com.saymk.cloud6x.minio.service;
 
-import com.saymk.cloud6x.minio.dto.ResourceInfoResponseDTO;
-import com.saymk.cloud6x.minio.model.MinioResource;
+import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import lombok.Cleanup;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
-import java.util.List;
+import java.io.OutputStream;
 
 @RequiredArgsConstructor
 public abstract class MinioService {
@@ -17,13 +20,36 @@ public abstract class MinioService {
     @Value("${minio.bucket-name}")
     protected String bucketName;
 
-    public abstract ResourceInfoResponseDTO objectStats(String fullPath);
+    @SneakyThrows
+    public void putObject(String path, MultipartFile file) {
+        @Cleanup InputStream stream = file.getInputStream();
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(path)
+                        .stream(stream, file.getSize(), -1)
+                        .build()
+        );
+    }
+
+    @SneakyThrows
+    protected InputStream getFileStream(String fullPath) {
+        return minioClient.getObject(
+                GetObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(fullPath)
+                        .build()
+        );
+    }
+
+
+
 
     public abstract void deleteObjectByPath(String path);
 
     public abstract void copyObject(String sourcePath, String targetPath);
 
-    public abstract InputStream readObject(String folderPath);
+    public abstract InputStream getResourceStream(String path);
 
     public abstract boolean objectExists(String path);
 
